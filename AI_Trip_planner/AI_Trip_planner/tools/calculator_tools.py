d219e73a79400dbacb352ebc69f80f75fbb81328
wadeedkhan1 <121788644+wadeedkhan1@users.noreply.github.com>
2025-06-23 16:38:44 +0500

    Delete AI_Trip_planner/AI_Trip_planner directory

--- AI_Trip_planner/AI_Trip_planner/tools/calculator_tools.py
+++
@@ -1,9 +0,0 @@
-from crewai.tools import tool
-
-@tool("Make a Calculator")
-def calculate(operation: str) -> str:
-    """Evaluate a basic math expression like 2+2 or 10/5."""
-    try:
-        return str(eval(operation))
-    except:
-        return "Error: Invalid operation"
